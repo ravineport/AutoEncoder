4e4bd08b6f7d5568234ee7f98f9b18095bdf30ac
taniTk <tanitk91@gmail.com>
2016-05-14 04:03:13 +0900

    Add affine function

--- auto_encoder.py
+++ auto_encoder.py
@@ -1,7 +1,14 @@
 def outer(x, y):
-    return [[xi*yj for yj in y] for xi in x]
+    return [[xi * yj for yj in y] for xi in x]
+
+def affine(x, W, b):
+    h = [sum([wij * xi for wij, xi in zip(wRow, x)]) for wRow in W]
+    return [hi+bi for hi, bi in zip(h, b)]
 
 if __name__ == '__main__':
     x = [1,2,3]
     y = [4,5,6,7]
-    print(outer(x,y))
+    W = [[1,2,3], [4,5,6], [7,8,9], [10,11,12]]
+    b = [13,14,15,16]
+    print(outer(x, y))
+    print(affine(x, W, b))
